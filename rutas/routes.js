bebf1e20fa65056af907efc4dbe9686836902845
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-06-06 23:06:14 -0600

    verify

--- rutas/routes.js
+++ rutas/routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { getAllUsu, addUsu, updateUsu, deleteUsu, getUsuarioById } from '../controlador/usuarioController.js';
 import { authMiddleware } from "../controlador/authMiddelware.js";
-import { login, logout } from '../controlador/authController.js';
+import { login, logout, verifyToken } from '../controlador/authController.js'; // Importar verifyToken
 
 const router = express.Router();
+
+// Rutas de autenticación (públicas)
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/verifyToken", verifyToken); // ← AGREGAR ESTA RUTA
 
 // Rutas para usuarios (protegidas por authMiddleware)
 router.get("/usuarios/getAll", authMiddleware, getAllUsu);
@@ -11,13 +16,9 @@ router.get("/usuarios/:usuId", authMiddleware, getUsuarioById);
 router.post("/usuarios/add", authMiddleware, addUsu);
 router.put("/usuarios/update/:usuId", authMiddleware, updateUsu);
 router.delete("/usuarios/delete/:usuId", authMiddleware, deleteUsu);
-router.post("/login", login);
-router.post("/logout", logout);
 
 router.get("/test", (req, res) => {
   res.send("¡El servidor funciona!");
 });
-
-
 
 export default router;
